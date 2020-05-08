a2770e40620c5fdd45fc78084229006e25172072
bmasles <burke.masles@smoothstack.com>
2020-05-08 11:56:39 -0400

    .

--- src/environments/URL.ts
+++ src/environments/URL.ts
@@ -1,5 +1,5 @@
 export const url = {
-    counter: 'https://UtopiaCounter-1584257611.us-east-1.elb.amazonaws.com/resource-server/v2/counter',
-    customer: 'https://onlineApi-1532901154.us-east-1.elb.amazonaws.com/online/1.0.0',
+    counter: 'https://counter.utopiaairlines.com/resource-server/v2/counter',
+    customer: 'https://online.utopiaairlines.com/online/1.0.0',
     agent: 'https://awl22d7v57.execute-api.us-east-1.amazonaws.com/production'
 }
